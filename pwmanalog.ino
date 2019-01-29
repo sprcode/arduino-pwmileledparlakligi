6bdf77f422d5517387d8291137ab4007dcecedd2
sprcode <47007407+sprcode@users.noreply.github.com>
2019-01-29 15:34:02 +0300

    Add files via upload

---
+++ pwmanalog.ino
@@ -0,0 +1,16 @@
+int led=3;
+void setup() {
+pinMode(led,OUTPUT);
+}
+void loop() {
+analogWrite(led,5);
+delay(100);
+analogWrite(led,15);
+delay(100);
+analogWrite(led,25);
+delay(100);
+analogWrite(led,35);
+delay(100);
+analogWrite(led,45);
+delay(100);
+}
